d8259f96ed24f14fc0fd5297d22069f785b7196a
weijia <weijia@weijiadeMacBook-Pro.local>
2020-03-22 16:06:20 +0800

    add text

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './App.less';
+
+export default function App () {
+  return (
+    <div className="app">
+      <p class="fonova" style={{ marginTop: 90 }}>Welcome to usage</p>
+      <p class="fonova" style={{ fontSize: 46 }}>MobTech React IDE!</p>
+    </div>
+  )
+}
--- src/main.js
+++ src/main.js
@@ -4,16 +4,7 @@ if (module.hot) module.hot.accept();
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Button } from 'antd';
-import 'antd/dist/antd.css';
-import './main.less';
 
-console.log(NODE_ENV);
+import App from './App';
 
-function App() {
-  return (
-    <div><Button>按钮</Button></div>
-  )
-}
-
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App/>, document.getElementById('app'));
